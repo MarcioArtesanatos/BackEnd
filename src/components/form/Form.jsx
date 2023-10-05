bdac6c39fc932219a003afa5410ffd7b131a03cd
hebertdev82 <hebertdev82@gmail.com>
2023-10-05 19:12:28 -0300

    visual

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -86,85 +86,117 @@ function ItemCadastro() {
   };
 
   return (
-    <div>
-      <h2>Cadastro de Produtos</h2>
-        <form onSubmit={handleSubmit}>
+    <div className="container-fluid">
+      <div className="row flex-nowrap">
 
-          <div class="mb-3">
-            <label class="form-label">Categoria:</label>
-            <input type="text" class="form-control" value={categoria} onChange={handleCategoriaChange} required />
-            <div class="form-text">Informe a categoria do seu produto</div>
-          </div>
+      <div className="col-auto px-0">
+      <div id="sidebar" className="collapse collapse-horizontal show border-end">
+            <div id="sidebar-nav" className="list-group border-0 rounded-0 text-sm-start min-vh-100">
+            <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i className="bi bi-bootstrap"></i> <span>Página Inicial</span> </a>
+                    <a href="#" className="list-group-item border-end-0 d-inline-block text-truncate" data-bs-parent="#sidebar"><i className="bi bi-bootstrap"></i> <span>Cadastro de Produtos</span> </a>
+                </div>
+            </div>
+      </div>
 
-          <div class="mb-3">
-            <label class="form-label">Produto:</label>
-            <input type="text" class="form-control" value={produto} onChange={handleProdutoChange} required />
-            <div class="form-text">Informe o nome do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Avaliação:</label>
-            <input type="text" class="form-control" value={avaliacao} onChange={handleAvaliacaoChange} required />
-            <div class="form-text">Dê uma avaliação ao seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Valor:</label>
-            <input type="text" class="form-control" value={valor} onChange={handleValorChange} required />
-            <div class="form-text">Informe o valor do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Cores:</label>
-            <input type="text" class="form-control" value={cores} onChange={handleCoresChange} required />
-            <div class="form-text">Informe as cores disponíveis do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Quantidade:</label>
-            <input type="text" class="form-control" value={quantidade} onChange={handleQuantidadeChange} required />
-            <div class="form-text">Informe a quantidade do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Peso:</label>
-            <input type="text" class="form-control" value={peso} onChange={handlePesoChange} required />
-            <div class="form-text">Informe o peso do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Largura:</label>
-            <input type="text" class="form-control" value={largura} onChange={handleLarguraChange} required />
-            <div class="form-text">Informe a largura do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Altura:</label>
-            <input type="text" class="form-control" value={altura} onChange={handleAlturaChange} required />
-            <div class="form-text">Informe a altura do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Comprimento:</label>
-            <input type="text" class="form-control" value={comprimento} onChange={handleComprimentoChange} required />
-            <div class="form-text">Informe o comprimento do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Descrição:</label>
-            <textarea class="form-control" value={descricao} rows={15} cols={60} onChange={handleDescricaoChange} required></textarea>
-            <div class="form-text">Informe a descrição completa do seu produto</div>
-          </div>
-
-          <div class="mb-3">
-            <label class="form-label">Imagens:</label>
-            <input type="file" class="form-control" multiple onChange={handleImagensChange} />
-            <div class="form-text">Envie as imagens do seu produto</div>
-          </div>
+        <main className="col ps-md-2 pt-2">
+          <a href="#" data-bs-target="#sidebar" data-bs-toggle="collapse" className="border rounded-3 p-1 text-decoration-none"><i className="bi bi-list bi-lg py-2 p-1"></i> Menu</a>
           
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
+          <div className="page-header pt-3">
+            <h2>Cadastro de Produtos</h2>
+          </div>
 
-        </form>
+
+          <div className="row">
+            <div className="col-12">
+              
+            <form onSubmit={handleSubmit}>
+
+<div className="mb-3">
+  <label className="form-label">Categoria:</label>
+  <input type="text" className="form-control" value={categoria} onChange={handleCategoriaChange} required />
+  <div className="form-text">Informe a categoria do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Produto:</label>
+  <input type="text" className="form-control" value={produto} onChange={handleProdutoChange} required />
+  <div className="form-text">Informe o nome do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Avaliação:</label>
+  <input type="text" className="form-control" value={avaliacao} onChange={handleAvaliacaoChange} required />
+  <div className="form-text">Dê uma avaliação ao seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Valor:</label>
+  <input type="text" className="form-control" value={valor} onChange={handleValorChange} required />
+  <div className="form-text">Informe o valor do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Cores:</label>
+  <input type="text" className="form-control" value={cores} onChange={handleCoresChange} required />
+  <div className="form-text">Informe as cores disponíveis do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Quantidade:</label>
+  <input type="text" className="form-control" value={quantidade} onChange={handleQuantidadeChange} required />
+  <div className="form-text">Informe a quantidade do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Peso:</label>
+  <input type="text" className="form-control" value={peso} onChange={handlePesoChange} required />
+  <div className="form-text">Informe o peso do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Largura:</label>
+  <input type="text" className="form-control" value={largura} onChange={handleLarguraChange} required />
+  <div className="form-text">Informe a largura do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Altura:</label>
+  <input type="text" className="form-control" value={altura} onChange={handleAlturaChange} required />
+  <div className="form-text">Informe a altura do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Comprimento:</label>
+  <input type="text" className="form-control" value={comprimento} onChange={handleComprimentoChange} required />
+  <div className="form-text">Informe o comprimento do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Descrição:</label>
+  <textarea className="form-control" value={descricao} rows={15} cols={60} onChange={handleDescricaoChange} required></textarea>
+  <div className="form-text">Informe a descrição completa do seu produto</div>
+</div>
+
+<div className="mb-3">
+  <label className="form-label">Imagens:</label>
+  <input type="file" className="form-control" multiple onChange={handleImagensChange} />
+  <div className="form-text">Envie as imagens do seu produto</div>
+</div>
+
+<button type="submit" className="btn btn-primary">Cadastrar</button>
+
+</form>
+                    
+              
+                </div>
+            </div>
+        
+      
+        
+      </main>
+      
+
+      </div>
     </div>
   );
 }
